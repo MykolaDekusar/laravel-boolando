e88f0bbbcef8cc24e937fc584b33b9a0a8f58fe7
MykolaDekusar <ndekusar97@gmail.com>
2024-07-06 12:15:07 +0200

    [UPD] Small fixes and improvements

--- resources/views/shared/about_main.blade.php
+++ resources/views/shared/about_main.blade.php
@@ -1,9 +1,9 @@
 <main>
-    <div class="container">
+    <div class="container height-main">
         <div>
             <h1>This is the about page, welcome</h1>
         </div>
-        <div >
+        <div>
             <h2><a href="/">Click me if you want to get back to the main page</a></h2>
         </div>
     </div>
